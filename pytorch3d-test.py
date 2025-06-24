5045a730f02442e7456b0f0d136520a89a6e90a8
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2025-06-24 10:42:33 -0700

    bare bones

---
+++ pytorch3d-test.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def main(argc, argv):
+    if argc != 2:
+        print("please specify an argument.")
+        return
+
+    if __name__ == "__main__":
+        main(len(sys.argv), sys.argv)
