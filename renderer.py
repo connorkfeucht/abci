1c35bca7b7909c1ba17dbda07230c68b9f97ec28
Connor Feucht <132318582+connorkfeucht@users.noreply.github.com>
2025-07-07 16:22:34 -0700

    added max separation to keep items more consistently spaced and clustered together

--- renderer.py
+++ renderer.py
@@ -6,6 +6,7 @@ import pyvista as pv # high level wrapper around VTK for building and rendering 
 import os
 import glob
 import random
+import math
 
 '''
 Things to implement:
@@ -17,7 +18,6 @@ Things to implement:
 - image level augmentation
 - in plane rotation DONE
 - multiple objects in a scene together DONE
-
 '''
 # plots meshes to output.png
 def plot_meshes(meshes):
@@ -81,7 +81,7 @@ def make_meshes(orig_dir, input_dir):
     return meshes
 
 # transforms meshes randomly in the scene.
-def transform_meshes(meshes, translate_range=(0,150), min_sep=0.0, max_trials=500): # TODO: ADD MAX SEPARATION TO KEEP ITEMS MORE CLUSTERED TOGETHER
+def transform_meshes(meshes, translate_range=(0,200), min_sep=0.0, max_sep=50, max_trials=1000):
     placed_bounds = []
     transformed_meshes = []
 
@@ -104,6 +104,14 @@ def transform_meshes(meshes, translate_range=(0,150), min_sep=0.0, max_trials=50
             mesh.rotate_x(random.randint(0,360), inplace=True)
             mesh.rotate_y(random.randint(0,360), inplace=True)
             mesh.rotate_z(random.randint(0,360), inplace=True)
+            
+            # enforce max_sep
+            if max_sep is not None and placed_bounds:
+                # ensure new mesh is not too far from any existing one
+                dists = [compute_distance(new_bounds, b) for b in placed_bounds]
+                if any(dist > max_sep for dist in dists):
+                    continue  # too far, retry
+
 
             # if new_bounds for mesh doesn't overlap other objects already placed, then add to new_bounds
             if all(not overlap(new_bounds, b, min_sep) for b in placed_bounds):
@@ -124,6 +132,34 @@ def overlap(b1, b2, min_sep=0.0): # b = (xmin, xmax, ymin, ymax, zmin, zmax)
     z_overlap = (b1[4] < b2[5] + min_sep) and (b1[5] + min_sep > b2[4])
 
     return x_overlap and y_overlap and z_overlap
+
+# computes the distance between two objects, assumes they do not overlap
+def compute_distance(b1, b2):
+    if b1[0] > b2[1]: # b1's xmin greater than b2's xmax
+        dx = b1[0] - b2[1] # dist = b1's xmin - b2's xmax
+    elif b2[0] > b1[1]: # b2's xmin greater than b1's xmax
+        dx = b2[0] - b1[1] # dist = b2's xmin - b1's xmax
+    else: # overlap
+        dx = 0
+
+    if b1[2] > b2[3]:
+        dy = b1[2] - b2[3]
+    elif b2[2] > b1[3]:
+        dy = b2[2] - b1[3]
+    else:
+        dy = 0
+
+    if b1[4] > b2[5]:
+        dz = b1[4] - b2[5]
+    elif b2[4] > b1[5]:
+        dz = b2[4] - b1[5]
+    else:
+        dz = 0
+    
+    euclidean_distance = math.sqrt(dx**2 + dy**2 + dz**2)
+    return euclidean_distance
+
+
 
 def main(argc, argv):
     if argc != 2:
